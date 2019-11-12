5f57f4bddeebc956b12fec4d8667ed107d41921e
Fire <fire@Fires-MacBook-Pro.local>
2019-11-13 01:15:15 +0200

    created tasks 2 & 3

--- src/main/java/RandomData.java
+++ src/main/java/RandomData.java
@@ -11,7 +11,18 @@ public class RandomData {
     static int ramdomInt(){
         int randomNum = ThreadLocalRandom
                 .current()
-                .nextInt(0, 50 + 1);
+                .nextInt(0, 81 + 1);
         return randomNum;
+    }
+
+    static int[] setArray(){
+        int[] arrVal = new int[10];
+
+        for(int i = 0; i<arrVal.length; i++){
+            arrVal[i] = RandomData.ramdomInt();
+            System.out.print("[ " + arrVal[i] + " ]");
+        }
+        System.out.println("size - " + arrVal.length);
+        return arrVal;
     }
 }
--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,24 +1,61 @@
 public class TestClass {
 
     public static void main(String[] args) {
+
+        //Fist task
+        //create object
         RandomData val = new RandomData();
 
+        //show value
         System.out.println("a = " + val.a + "; "
                 + "b = " + val.b + "; "
                 + "c = " + val.c + "; "
                 + "d = " + val.d + "; "
                 + "e = " + val.e + "; ");
 
-        double res1 = (val.a+val.b)/(val.b-val.d)*2;
-        double res2 = val.a+2*2-12/val.a*(val.b-val.c);
+        //realize formuls
+        double res1 = (val.a + val.b) / (val.b - val.d) * 2;
+        double res2 = val.a + 2 * 2 - 12 / val.a * (val.b - val.c);
         double res3 = val.b-11*val.c-val.a;
         double res4 = val.a * (val.b + val.c + val.d) - val.e / val.c + val.a;
         double res5 = val.a + val.b * (val.a - val.b) / 12;
 
+        //show results
         System.out.println("result of formula #1 = " + res1);
         System.out.println("result of formula #2 = " + res2);
         System.out.println("result of formula #3 = " + res3);
         System.out.println("result of formula #4 = " + res4);
         System.out.println("result of formula #5 = " + res5);
+
+        //Second task
+        //show date
+        for(int i : RandomData.setArray()){
+            if(i % 2 == 0){
+                System.out.println("/2 = " + i);
+            } else if (i % 3 ==0){
+                System.out.println("/3 = " + i);
+            } else if (i % 5 == 0) {
+                System.out.println("/5 = " + i);
+            } else if (i % 7 == 0) {
+                System.out.println("/7 = " + i);
+            }
+        }
+
+        //Threth task
+        int[] arr = RandomData.setArray();
+
+        for (int i = arr.length-1; i > 0 ; i--) {
+            for (int j = 0 ; j < i ; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        for(int i =0; i<arr.length; i++) {
+            System.out.print("[ " + arr[i] + " ]");
+        }
     }
 }
