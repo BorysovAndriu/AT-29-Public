5f97c08cca0e3ed6aae2462511a922bbff279e3e
Fire <fire@Fires-MacBook-Pro.local>
2019-11-12 23:49:32 +0200

    updeted 5 formuls

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,59 +1,24 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import org.w3c.dom.ls.LSOutput;
-
 public class TestClass {
+
     public static void main(String[] args) {
-        System.out.println("result of formula #1 = " + formula1());
-        System.out.println("result of formula #2 = " + formula2());
-        System.out.println("result of formula #3 = " + formula3());
-        System.out.println("result of formula #4 = " + formula4());
-        System.out.println("result of formula #5 = " + formula5());
-
-    }
-
-    static double formula1(){
         RandomData val = new RandomData();
-        System.out.println("a = " + val.a + "; "
-                + "b = " + val.b + "; "
-                + "d = " + val.d + "; ");
 
-        return (val.a+val.b)/(val.b-val.d)*2;
-    }
-
-    static double formula2(){
-        RandomData val = new RandomData();
-        System.out.println("a = " + val.a + "; "
-        + "b = " + val.b + "; "
-        + "c = " + val.c + "; ");
-
-        return val.a+2*2-12/val.a*(val.b-val.c);
-    }
-
-    static double formula3(){
-        RandomData val = new RandomData();
-        System.out.println("a = " + val.a + "; "
-        + "b = " + val.b + "; "
-        + "c = " + val.c + "; ");
-
-        return val.b-11*val.c-val.a;
-    }
-
-    static double formula4() {
-        RandomData val = new RandomData();
         System.out.println("a = " + val.a + "; "
                 + "b = " + val.b + "; "
                 + "c = " + val.c + "; "
                 + "d = " + val.d + "; "
                 + "e = " + val.e + "; ");
 
-        return val.a * (val.b + val.c + val.d) - val.e / val.c + val.a;
-    }
+        double res1 = (val.a+val.b)/(val.b-val.d)*2;
+        double res2 = val.a+2*2-12/val.a*(val.b-val.c);
+        double res3 = val.b-11*val.c-val.a;
+        double res4 = val.a * (val.b + val.c + val.d) - val.e / val.c + val.a;
+        double res5 = val.a + val.b * (val.a - val.b) / 12;
 
-    static double formula5() {
-        RandomData val = new RandomData();
-        System.out.println("a = " + val.a + "; "
-                + "b = " + val.b + "; ");
-
-        return val.a + val.b * (val.a - val.b) / 12;
+        System.out.println("result of formula #1 = " + res1);
+        System.out.println("result of formula #2 = " + res2);
+        System.out.println("result of formula #3 = " + res3);
+        System.out.println("result of formula #4 = " + res4);
+        System.out.println("result of formula #5 = " + res5);
     }
 }
