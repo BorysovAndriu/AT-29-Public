5cb42413f0ba365c2d85c17902d1c24e4b5be23f
Fire <fire@Fires-MacBook-Pro.local>
2019-11-13 20:03:49 +0200

    OOP java

---
+++ src/main/java/OOP/Main.java
@@ -0,0 +1,29 @@
+package OOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*Person person = new Person("Johm", 12);
+        Person person1 = person.getPerson();
+
+        System.out.println(person1.age);
+        *//*person.name = "John";
+
+        System.out.println(person.name);
+        System.out.println(person.age);
+
+        person.whoAmI();
+        System.out.println(person.getAge());*//*
+
+        Person.foo();*/
+
+        int i = 0;
+        while(i<10){
+            i++;
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
