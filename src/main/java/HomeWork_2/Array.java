1999279401cfcb83b81e973500bb03a993502f22
fire <andriu.test.post.1@gmail.com>
2019-11-16 20:21:27 +0200

    homeWork2 - update name method

--- src/main/java/HomeWork_2/Array.java
+++ src/main/java/HomeWork_2/Array.java
@@ -115,7 +115,7 @@ public class Array {
         return newArray;
     }
 
-    public void indexPAirValueArray(){
+    public void indexPairValueArray(){
         for (int i = 0; i<arr.length; i++){
             if (arr[i]%2 == 0){
                 System.out.print(arr[i] + " index(" + i + ") ");
